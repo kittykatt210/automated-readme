bf2bc03d524b06a75d9998efb32b1b46b42f9971
kittykatt210 <keperry30@icloud.com>
2023-10-22 10:58:29 -0500

    Added questions and setup initial layout for markdown file

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const licenses = ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause Simplified License", "BSD 3-Clause New or Revised License", "BoostSoftwareLicense 1.0", "Creative Commons Zero V1.0 Universal", "Eclispse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
+
+const questions = [
+    {type: 'input', message: 'Enter the title of your project:', name: 'projTitle', validate: (value) => {if(value) {return true} else { return 'Please enter a title for your project'}}},
+    {type: 'input', message: 'Enter a description of your project (the what, why, and how):', name: 'description', validate: (value) => {if(value) {return true} else { return 'Please enter a description for your project'}}},
+    {type: 'input', message: 'Enter the installation instructions:', name: 'install', validate: (value) => {if(value) {return true} else { return 'Please enter install instructions for your project'}}},
+    {type: 'input', message: 'Enter usage instructions:', name: 'usage', validate: (value) => {if(value) {return true} else { return 'Please enter usage intructions for your project'}}},
+    {type: 'input', message: 'Enter any contributors, to include tutorial websites:', name: 'contributors'},
+    {type: 'input', message: 'Enter any test instructions:', name: 'testInst', },
+    {type: 'list', message: 'Select the license used for your project:', name: 'license', choices: licenses},
+    {type: 'input', message: 'Enter your github username:', name: 'github', validate: (value) => {if(value) {return true} else { return 'Please enter your github username'}}},
+    {type: 'input', message: 'Enter your email address:', name: 'email', validate: (value) => {if(value) {return true} else { return 'Please enter your email'}}}
+];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+inquirer.prompt(questions);
+// Function call to initialize app
+init();
